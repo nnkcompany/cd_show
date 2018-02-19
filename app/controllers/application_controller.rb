dd52d16590affcc2758ebc08b7e433bcfd0d6456
Hiroki.K <po.kitagawa61@gmail.com>
2018-02-19 11:02:16 +0000

    user all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-	def after_sign_in_path_for(resource)
-  		root_path
-  	end
-
-	def after_sign_up_path_for(resource)
-  		root_path
-	end
-
-	def after_log_out_path_for(resource)
-  		root_path
-	end
+  # deviseではサインアップ時にメールアドレスとパスワードのみを受け取るようにストロングパラメーターで設定してあるのでキーを追加しても許可されません。
+  # そこで登場するのがconfigure_permitted_parametersメソッド,deviseで利用出来るパラメーターを設定しますよ。という意味。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
 
 
+  def after_sign_in_path_for(resource)
+     root_path
+  end
+
+
+
+
+  # ストロングパラメーター：アサインメント脆弱性のため
+
+  protected
+  # 以下のパラメーターを追加します
+  def configure_permitted_parameters
+  	devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :last_name_kana, :first_name, :first_name_kana, :zip, :address, :tell, :id])
+  end
 end
