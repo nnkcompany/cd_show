fe47c0da616ffbe706b78ec05a315bc6e87fc749
Hiroki.K <po.kitagawa61@gmail.com>
2018-02-20 08:26:47 +0000

    add user_delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180214054958) do
+ActiveRecord::Schema.define(version: 20180220071049) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(version: 20180214054958) do
     t.string "tell"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
