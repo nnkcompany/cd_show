5617d8e93674a0305cc60e24bc14a798a1f07961
Your Name <asa_yhk@hotmail.com>
2018-02-24 16:03:04 +0900

    up to date product_header/CSS

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20180222090755) do
-
 
   create_table "addresses", force: :cascade do |t|
     t.integer "user_id"
@@ -126,9 +124,9 @@ ActiveRecord::Schema.define(version: 20180222090755) do
     t.string "zip"
     t.string "address"
     t.string "tell"
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
