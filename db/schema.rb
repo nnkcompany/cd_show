e8e46713844d939d21b2fe6dbd4ef9d5fd439ac8
Hiroki.K <po.kitagawa61@gmail.com>
2018-02-28 06:10:41 +0000

    try1

--- db/schema.rb
+++ db/schema.rb
@@ -129,9 +129,9 @@ ActiveRecord::Schema.define(version: 20180227053158) do
     t.string "zip"
     t.string "address"
     t.string "tell"
+    t.datetime "deleted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
