5bffc7fd5b74fa7098ceae2ada9800474b8ee3f2
hayato1118 <hayato.nakamura1118@gmail.com>
2018-03-01 11:54:41 +0000

    all_fin

--- app/models/product_cart.rb
+++ app/models/product_cart.rb
@@ -4,13 +4,5 @@ class ProductCart < ApplicationRecord
 	belongs_to :product
 	belongs_to :cart
 
-validates :product_id, uniqueness: true
-
-# def create
-# 	@book_cart = BookCart.new(book_cart_params)
-# end
-
-# def destroy
-# end
 
 end
